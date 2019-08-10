5f0eaaa76fa04348e1685dd350dc99c223eecc5b
Atharva Naik <50011593+atharvadnaik@users.noreply.github.com>
2019-08-10 21:59:48 +0530

    Code which diverges any positive integer to 1

---
+++ DivergeTo1.py
@@ -0,0 +1,14 @@
+x = int(input("Enter No. "))
+t = 0
+if x == 1:
+    print(t)
+else:    
+    while x != 1:
+        if x % 2 == 0:
+            x = x/2
+            t += 1
+        else:
+            x = 3*x+1
+            t +=1
+        print(x)
+    print(f"The number of iterations is {t}")
