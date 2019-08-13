acad915a283f01d86ae0b78377245ffead4cb4ef
Atharva Naik <50011593+atharvadnaik@users.noreply.github.com>
2019-08-13 17:41:49 +0530

    Right Half Pyramid

---
+++ Pyramid/Star.py
@@ -0,0 +1,9 @@
+t = int(input("Enter No. of Rows "))
+k = 2*t - 1
+for i in range (1,t+1):
+    for j in range(1,k):
+        print(end = " ")
+    k = k -2
+    for j in range(1, i+1):
+        print("* ",end = '')
+    print()
