41b4af1e8bdaa942ed935db71e6c3e85aff773e1
Atharva Naik <50011593+atharvadnaik@users.noreply.github.com>
2020-06-21 23:25:20 +0530

    Diverge natural number to 1
    Added additional check for natural number

--- DivergeTo1.py
+++ DivergeTo1.py
@@ -1,8 +1,20 @@
-x = int(input("Enter No. "))
+'''
+Concept:
+You have any natural number n.
+If n is even: divide it by 2
+If n is odd: multiply it by 3 and add 1
+Eventually these process will continue till number diverges to 1
+'''
+while True:
+    x = int(input("Enter any natural number "))
+    if x <= 0:
+        print("Natural Numbers are positive integers greater than 1")
+        continue
+    else:
+        break
 t = 0
 if x == 1:
-    print(t)
-    print("The number of iterations is 1")
+    print("Single iteration and we have 1...Cheers!")
 else:    
     while x != 1:
         if x % 2 == 0:
@@ -11,5 +23,5 @@ else:
         else:
             x = 3*x+1
             t +=1
-        print(x)
+        print(int(x))
     print(f"The number of iterations is {t}")
